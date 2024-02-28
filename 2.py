3e3cb191e09ba7eb7d1d0d819e465c771f34a3a2
pavlovdmitrii4871 <59319885+pavlovdmitrii4871@users.noreply.github.com>
2024-02-28 11:22:19 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -7,19 +7,19 @@ def generateRandomMatrix(m=None, n=None, min_limit=0, max_limit=100):
     for i in range(m):
         row = []
         for j in range(n):
-            num = random.randint(min_limit, max_limit)  # случайное значение в заданном диапазоне
+            num = random.randint(min_limit, max_limit)  
             row.append(num)
         matrix.append(row)
 
     return matrix
 
 
-m = int(input("Введите количество строк матрицы: "))
-n = int(input("Введите количество столбцов матрицы: "))
-min_limit = int(input("Введите минимальное значение: "))
-max_limit = int(input("Введите максимальное значение: "))
+m = int(input("Количество строк матрицы: "))
+n = int(input("Количество столбцов матрицы: "))
+min_limit = int(input("Минимальное значение элементов в матрице: "))
+max_limit = int(input("Максимальное значение элементов в матрице: "))
 
 random_matrix = generateRandomMatrix(m, n, min_limit, max_limit)
-print("Вывод матрицы:")
+print("Результат:")
 for row in random_matrix:
     print(row)
